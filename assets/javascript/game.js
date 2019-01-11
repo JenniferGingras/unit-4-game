5b05359bc54d5488af1105f2a2460d4d0f3d954b
Jennifer Gingras <jennifergingras@tests-MacBook-Pro-2.local>
2019-01-10 20:05:33 -0500

    mostly done, a bit of UX to still work on

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,20 +11,45 @@ $(document).ready(function () {
   var winCounter = 0;
   var lossCounter = 0;
 
-  // holds button numbers that computer picks
-  var buttonBird;
-  var buttonFox;
-  var buttonSquirrel;
-  var buttonMouse;
-
-  var gamePlaying = false;
+  // var gamePlay = false;
 
   // FUNCTIONS
   //-----------------
 
-  // SET UP THE GAME
-  function gameSet() {
-    gamePlaying = true
+  // CHECK FOR A WIN OR LOSS
+  var checkGame = function () {
+    // if the userSum = randomNum the game is won
+    if (userSum === randomNum) {
+      // gamePlay = false;
+      // update win counter plus 1
+      winCounter++;
+      $('#winCounter').html(winCounter);
+      console.log('win');
+      // update the message with 'Yay! You won! Do you want to play again?'
+      $('#message').text('Yay! You won! Want to play again?');
+      // update button to say 'play again?'
+      $('#playButton').text('Play Again?');
+
+    }
+    // if userSum > randomNum the game is lost
+    else if (userSum > randomNum) {
+      //  gamePlay = false;
+      // update lose counter plus 1
+      lossCounter++;
+      $('#lossCounter').html(lossCounter);
+      console.log('lose');
+      // update the message with 'Oh no! You lost! Would you like to try again?'
+      $('#message').text('Oh no! You lost! Want to try again?');
+      // update button to say 'play again'
+      $('#playButton').text('Play Again?');
+    } else {
+      //  gamePlay = true;
+    }
+  };
+
+  // SET THE GAME FOR PLAYING
+  var gameSet = function () {
+    // gamePlay = true;
     // Set sum to 0
     userSum = 0;
     $('#userTotal').html(userSum);
@@ -34,95 +59,37 @@ $(document).ready(function () {
     console.log(randomNum);
     $('#randomNumber').html(randomNum);
 
-
     // generate a number for each of the buttons
-    buttonBird = Math.floor(Math.random() * 11) + 1;
-    console.log(buttonBird);
-    buttonFox = Math.floor(Math.random() * 11) + 1;
-    console.log(buttonFox);
-    buttonSquirrel = Math.floor(Math.random() * 11) + 1;
-    console.log(buttonSquirrel);
-    buttonMouse = Math.floor(Math.random() * 11) + 1;
-    console.log(buttonMouse);
-  }
-  // call startGame just to see it console logged
-  // gameSet();
-
-
-  // CHECK END GAME
-  function checkGame() {
-    // if the userSum = randomNum the game is won
-    if (userSum === randomNum) {
-      // update win counter plus 1
-      winCounter++;
-      $('#winCounter').html(winCounter);
-      // update the message with 'Yay! You won! Do you want to play again?'
-      $('#message').text('Yay! You won! Want to play again?');
-      // update button to say 'play again?'
-      $('#playButton').text('Play Again?');
-    }
-    // if userSum > randomNum the game is lost
-    else if (userSum > randomNum) {
-      // update lose counter plus 1
-      lossCounter++;
-      $('#lossCounter').html(lossCounter);
-      // update the message with 'Oh no! You lost! Would you like to try again?'
-      $('#message').text('Oh no! You lost! Want to try again?');
-      // update button to say 'play again'
-      $('#playButton').text('Play Again?');
-    }
-    // if userSum < randomNum game is still in play
-    else if (userSum < randomNum) {
-      gamePlaying = true;
-    }
+    // use jquery to select html buttons and store the number
+    $("#buttonBird").data("value", Math.floor(Math.random() * 11) + 1);
+    console.log('bird: ' + $('#buttonBird').data('value'));
+    $("#buttonFox").data("value", Math.floor(Math.random() * 11) + 1);
+    console.log('fox: ' + $('#buttonFox').data('value'));
+    $("#buttonSquirrel").data("value", Math.floor(Math.random() * 11) + 1);
+    console.log('squirrel: ' + $('#buttonSquirrel').data('value'));
+    $("#buttonMouse").data("value", Math.floor(Math.random() * 11) + 1);
+    console.log('mouse: ' + $('#buttonMouse').data('value'));
   };
 
   // BUTTON CLICKS
+
+  // Play Game Button
   $('#playButton').on('click', function () {
     gameSet();
   });
 
-  // FIX SO THAT YOU CAN'T CLICK ANIMAL BUTTONS UNTIL GAME IS PLAYING
-  // THERE'S GOTTA BE A WAY TO COMBINE ALL BUTTONS
-  // --------------------------------------------------
-  $("#buttonFox").on("click", function () {
+  // Animal Buttons
+  // if (gamePlay === true) {
+  $(".buttonImage").on("click", function () {
     // add value of button to userSum
-    userSum = userSum + buttonFox;
-    console.log(userSum);
+    userSum = userSum + $(this).data('value');
+    console.log($(this).data('value'));
     // update to totalSum in html
     $('#userTotal').text(userSum);
     // call checkGame function to check if the game is over 
     checkGame();
   });
+  // };
 
-  $("#buttonBird").on("click", function () {
-    // add value of button to userSum
-    userSum = userSum + buttonBird;
-    console.log(userSum);
-    // update to totalSum in html
-    $('#userTotal').text(userSum);
-    // call checkGame function to check if the game is over 
-    checkGame();
-  });
 
-  $("#buttonSquirrel").on("click", function () {
-    // add value of button to userSum
-    userSum = userSum + buttonSquirrel;
-    console.log(userSum);
-    // update to totalSum in html
-    $('#userTotal').text(userSum);
-    // call checkGame function to check if the game is over 
-    checkGame();
-  });
-
-  $("#buttonMouse").on("click", function () {
-    // add value of button to userSum
-    userSum = userSum + buttonMouse;
-    console.log(userSum);
-    // update to totalSum in html
-    $('#userTotal').text(userSum);
-    // call checkGame function to check if the game is over 
-    checkGame();
-  });
-
-})
+});
